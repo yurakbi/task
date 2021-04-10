313e3ab9373196136d59d3ef9b13dbe6a57b1058
yura <yura04091@gmail.com>
2021-04-10 20:22:08 +0300

    cycles 2

--- script.js
+++ script.js
@@ -46,6 +46,44 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+
+// цикли while
+// let i = 0;
+// while (i < 2 ) {
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         } else {
+//         console.log('error');
+//         i--;
+                        
+//         }
+
+// }
+
+// цикли do
+// let i = 0;
+// do {
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;             
+//     }
+// } while (i < 2);
+
+
+
+
+
 if (personalMovieDB.count < 10 ) {
     console.log('Просмотрено очень мало фильмов');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -55,6 +93,8 @@ if (personalMovieDB.count < 10 ) {
 } else {
     console.log('Произошла ошибка');
 }
+
+
 
 
     
